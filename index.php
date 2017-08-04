da2672e04edf9f32d651b3ce779337a0fc067e3a
Chriss Haddad <chriss.hadad@gmail.com>
2017-08-04 17:23:25 +0300

    Added NavBar And Slideshow

--- index.php
+++ index.php
@@ -17,10 +17,49 @@ require_once("./dbManagers/db-manager-sqlite.php");
     <div class="container">
 
 
+        <header>
+            <div class="navigation-bar">
+                <ul class="navigation-menu">
+
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+
+                </ul>
+            </div>
+        </header>
+
+        <div class="slideshow-container">
+            <div class="mySlides fade">
+                <img class="slider-image" src="images/1.jpg" style="width:100%" />
+            </div>
+
+            <div class="mySlides fade">
+                <img class="slider-image" src="images/2.jpg" style="width:100%" />
+            </div>
+
+            <div class="mySlides fade">
+                <img class="slider-image" src="images/3.jpg" style="width:100%" />
+            </div>
+
+            <div class="mySlides fade">
+                <img class="slider-image" src="images/4.jpg" style="width:100%" />
+            </div>
+
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br />
+
 
     </div>
 
 
 </body>
+
+<script src="slideshow.js"></script>
 
 </html>
