dd5643734ded3bc5be3e3fcc1a9674ea4247dd24
Chriss Haddad <chriss.hadad@gmail.com>
2017-08-22 17:07:40 +0300

    Added functions to person-manager

--- dbManagers/person-manager.php
+++ dbManagers/person-manager.php
@@ -10,10 +10,10 @@ class PersonManager
     
     public function addPerson($id,$name, $lastName, $gender, $email, $telephone, $userType,$username,$password)
     {
-        $insert    = "INSERT INTO person (id,name,lastName,gender,email,telephone,userType,username,password) VALUES (:id,:name,:lastName,:gender,:email,:telephone,:userType,:username,:password)";
+        $date = date("Y/m/d");
+        $insert    = "INSERT INTO person (id,name,lastName,gender,email,telephone,userType,username,password,date) VALUES (:id,:name,:lastName,:gender,:email,:telephone,:userType,:username,:password,:date)";
         $statement = $this->db->prepare($insert);
-        $statement->execute([':id'=>$id, ':name'=> $name,':lastName'=> $lastName,':gender'=> $gender,':email'=> $email,':telephone'=> $telephone,':userType'=> $userType,':username'=> $username,':password'=> $password ]
-        );
+        $statement->execute([':id'=>$id, ':name'=> $name,':lastName'=> $lastName,':gender'=> $gender,':email'=> $email,':telephone'=> $telephone,':userType'=> $userType,':username'=> $username,':password'=> $password,':date'=>$date]);
     }
     
     public function deletePerson($id)
@@ -23,31 +23,98 @@ class PersonManager
         $statement->execute([':id'=>$id]);
     }
 
-    public function editPerson($id,$name, $lastName, $gender, $email, $telephone, $userType){
-        $update = "UPDATE person SET name=:name, lastName=:lastName, gender=:gender, email=:email,telephone=:telephone WHERE id=:id";
+    public function editPerson($id,$name, $lastName, $gender, $email, $telephone, $userType,$username,$password){
+        $update = "UPDATE person SET name=:name, lastName=:lastName, gender=:gender, email=:email,telephone=:telephone,username=:username,password=:password WHERE id=:id";
         $statement = $this->db->prepare($update);
-        $statement->execute([':id'=> $id,':name'=> $name,':lastName'=> $lastName,':gender'=> $gender,':email'=> $email,':telephone'=> $telephone,':userType'=> $userType]);
+        $statement->execute([':id'=> $id,':name'=> $name,':lastName'=> $lastName,':gender'=> $gender,':email'=> $email,':telephone'=> $telephone,':userType'=> $userType,':username'=> $username,':password'=> $password]);
     }
 
     public function getPersonData($id){
 
         $person = "SELECT * FROM person WHERE id=:id";
-        $statement = $this->db->prepare($article);
+        $statement = $this->db->prepare($person);
         $statement->execute([':id' => $id]);
         $personData = $statement->fetchAll()[0];
 
         return $personData;
     }
 
-    public function personExists($id){
+
+    public function getPeopleByType($usertype){
+
+        $people = "SELECT * FROM person WHERE usertype=:usertype";
+        $statement = $this->db->prepare($people);
+        $statement->execute([':usertype' => $usertype]);
+        $peopleData = $statement->fetchAll()[0];
+
+        return $peopleData;
+    }
+
+    public function getPeopleByName($name,$lastName){
+
+        $people = "SELECT * FROM person WHERE name LIKE '%:name%' AND lastName LIKE '%:lastName%'";
+        $statement = $this->db->prepare($people);
+        $statement->execute([':name'=>$name,':lastName'=>$lastName]);
+        $peopleData = $statement->fetchAll();
+
+        return $peopleData;
+    }
+
+
+    public function personIdExists($id){
 
         $success=$this->getPersonData($id);
         if (empty($success)) return "false";
         else return "true";
 
     }
+
+    public function nameExists($name, $lastName){
+
+	    if (!empty($name) || !empty($lastName))
+	    	{
+	    	$success = $this->getPeopleByName($name, $lastName);
+	    	if (empty($success)) return "false";
+	    	  else return "true";
+	    	}
+
+	    return "false";
+	}
+
+    public function typeExists($usertype){
+        $success=$this->getPeopleByType($usertype);
+        if (empty($success)) return "false";
+        else return "true";
+    }
+
     
-    
+    public function getErrorPersonId($id){
+        if($this->personExists($id)=="false"){
+            return "Invalid Id or Id does not exist";
+        } else {
+            return "All Good No Errors";
+        }
+    }
+
+    public function getErrorType($usertype){
+        if($this->typeExists($usertype)=="false"){
+            return "Invalid Type or Type does not exist";
+        } else {
+            return "All Good No Errors";
+        }
+    }
+
+    public function getErrorName($name,$lastName){
+        if (empty($name) && empty($lastName)){
+            return "You Need To Input At Least A First Name or A Last Name";
+        }else if($this->nameExists($name, $lastName)=="false") {
+            return "Name Does Not Exist";
+        }
+        else return "All Good No Errors";
+    }
+
+
+
 }
 
 ?>
--- router.php
+++ router.php
@@ -1,45 +1,42 @@
 <?php
-
-
 require_once("./dbManagers/article-manager.php");
 
+require_once("./dbManagers/person-manager.php");
 
 class Router
 {
-
     public $db;
-
+    
     public function __construct(PDO $db)
     {
-        $this->db = $db;
+        $this->db         = $db;
         $this->articleMan = new ArticleManager($db);
-        $this->personMan = new PersonManager($db);
+        $this->personMan  = new PersonManager($db);
     }
-
+    
     public function getUrl()
     {
         return "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     }
-
+    
     public function getPathArray()
     {
         return explode('/', trim(parse_url($this->getUrl(), PHP_URL_PATH), "/"));
     }
-
+    
     public function sqlToArray($sql)
     {
         return $sql->fetchAll();
     }
-
+    
     public function route()
     {
-        $path = $this->getPathArray();
+        $path      = $this->getPathArray();
         $jsonArray = array();
-
-
         if ($path[0] == 'api') {
+            
+            
             if ($path[1] == "home") {
-
                 if (isset($_GET["page"])) {
                     $page = $_GET["page"];
                     if ($page < 1) {
@@ -48,108 +45,132 @@ class Router
                 } else {
                     $page = 1;
                 }
-
-                $limit = $_GET["limit"];
-
-                $data = $this->articleMan->getPageData($page, $limit);
-                $success = $this->articleMan->successPage($page, $limit);
-                $previous = $this->articleMan->previousPages($page, $limit);
-                $next = $this->articleMan->nextPages($page, $limit);
-                $error = $this->articleMan->getErrorPage($page, $limit);
-                $lastPage = $this->articleMan->getNumberOfPages($limit);
-
-                $jsonArray['success'] = $success;
-                $jsonArray['data'] = $data;
+                
+                $limit                 = $_GET["limit"];
+                $data                  = $this->articleMan->getPageData($page, $limit);
+                $success               = $this->articleMan->successPage($page, $limit);
+                $previous              = $this->articleMan->previousPages($page, $limit);
+                $next                  = $this->articleMan->nextPages($page, $limit);
+                $error                 = $this->articleMan->getErrorPage($page, $limit);
+                $lastPage              = $this->articleMan->getNumberOfPages($limit);
+                $jsonArray['success']  = $success;
+                $jsonArray['data']     = $data;
                 $jsonArray['previous'] = $previous;
-                $jsonArray['next'] = $next;
-                $jsonArray['last'] = $lastPage;
-                $jsonArray['error'] = $error;
-
-            } else if ($path[1] == "articles") {
-
-                if($path[2]=="add"){
-
-                $title=$_POST['title'];
-                $text=$_POST['text'];
-                $files=$_FILES['files'];
-
-                
-                $this->articleMan->addArticle($title, $text,$files);
-                $success = $this->articleMan->successAddArticle($title,$text,$files);
-                $error = $this->articleMan->getErrorAddArticle($title,$text,$files);
-
-                $jsonArray['success'] = $success;
-                $jsonArray['error'] = $error;
-
-                } else if($path[2]=="edit"){
-
-
-                $id=$_POST['id'];
-                $title=$_POST['title'];
-                $text=$_POST['text'];        
-
-                $this->articleMan->editArticle($id, $title, $text);
-                $success = $this->articleMan->successEditArticle($title,$text);
-                $error = $this->articleMan->getErrorEditArticle($title,$text);
-
-                $jsonArray['success'] = $success;
-                $jsonArray['error'] = $error;
-                    
-                } else if($path[2]=="delete"){
-
-                
-                $id=$_GET['id'];
-            
-                $this->articleMan->deleteArticle($id);
-                
-                
-                } else {
-
-                $id = $_GET["id"];
-
-                $success = $this->articleMan->successArticle($id);
-                $data = $this->articleMan->getArticleData($id);
-                $error = $this->articleMan->getErrorArticle($id);
-                $next = $this->articleMan->nextArticle($id);
-                $previous = $this->articleMan->previousArticle($id);
-
-                $jsonArray['success'] = $success;
-                $jsonArray['data'] = $data;
-                $jsonArray['next'] = $next;
-                $jsonArray['previous'] = $previous;
-                $jsonArray['error'] = $error;
-
-                }
-
-            } else if($path[1]=="person"){
-
-                $id = $_GET["id"];
-
-                $success = $this->personMan->personExists($id);
-                $data = $this->personMan->getPersonData($id);
-
-                $jsonArray['success'] = $success;
-                $jsonArray['data'] = $data;
+                $jsonArray['next']     = $next;
+                $jsonArray['last']     = $lastPage;
+                $jsonArray['error']    = $error;
             }
             
+            else if ($path[1] == "articles") {
+                if ($path[2] == "add") {
+                    $title = $_POST['title'];
+                    $text  = $_POST['text'];
+                    $files = $_FILES['files'];
+                    $this->articleMan->addArticle($title, $text, $files);
+                    $success              = $this->articleMan->successAddArticle($title, $text, $files);
+                    $error                = $this->articleMan->getErrorAddArticle($title, $text, $files);
+                    $jsonArray['success'] = $success;
+                    $jsonArray['error']   = $error;
+                } else if ($path[2] == "edit") {
+                    $id    = $_POST['id'];
+                    $title = $_POST['title'];
+                    $text  = $_POST['text'];
+                    $this->articleMan->editArticle($id, $title, $text);
+                    $success              = $this->articleMan->successEditArticle($title, $text);
+                    $error                = $this->articleMan->getErrorEditArticle($title, $text);
+                    $jsonArray['success'] = $success;
+                    $jsonArray['error']   = $error;
+                } else if ($path[2] == "delete") {
+                    $id = $_GET['id'];
+                    $this->articleMan->deleteArticle($id);
+                } else {
+                    $id                    = $_GET["id"];
+                    $success               = $this->articleMan->successArticle($id);
+                    $data                  = $this->articleMan->getArticleData($id);
+                    $error                 = $this->articleMan->getErrorArticle($id);
+                    $next                  = $this->articleMan->nextArticle($id);
+                    $previous              = $this->articleMan->previousArticle($id);
+                    $jsonArray['success']  = $success;
+                    $jsonArray['data']     = $data;
+                    $jsonArray['next']     = $next;
+                    $jsonArray['previous'] = $previous;
+                    $jsonArray['error']    = $error;
+                }
+                
+                // Change GET to POST after testing
+                
+            } else if ($path[1] == "person") {
+                
+                if ($path[2] == "add") {
+                    
+                    $id        = $_POST['id'];
+                    $name      = $_POST['name'];
+                    $lastName  = $_POST['lastName'];
+                    $gender    = $_POST['gender'];
+                    $email     = $_POST['email'];
+                    $telephone = $_POST['telephone'];
+                    $userType  = $_POST['userType'];
+                    $username  = $_POST['username'];
+                    $password  = $_POST['password'];
+                    
+                    $this->personMan->addPerson($id, $name, $lastName, $gender, $email, $telephone, $userType, $username, $password);
+                    
+                } else if ($path[2] == "edit") {
+                    
+                    $id        = $_POST['id'];
+                    $name      = $_POST['name'];
+                    $lastName  = $_POST['lastName'];
+                    $gender    = $_POST['gender'];
+                    $email     = $_POST['email'];
+                    $telephone = $_POST['telephone'];
+                    $userType  = $_POST['userType'];
+                    $username  = $_POST['username'];
+                    $password  = $_POST['password'];
+                    
+                    $this->personMan->editPerson($id, $name, $lastName, $gender, $email, $telephone, $userType, $username, $password);
+                    
+                } else if ($path[2] == "delete") {
+                    
+                    $id = $_POST['id'];
+                    
+                    $this->personMan->editPerson($id);
+                    
+                } else if($path[2]==""){
 
+                }
+                
+                
+                else {
+                    $id                   = $_GET["id"];
+                    $success              = $this->personMan->personExists($id);
+                    $data                 = $this->personMan->getPersonData($id);
+                    $error                = $this->personMan->getErrorPerson($id);
+                    $jsonArray['success'] = $success;
+                    $jsonArray['data']    = $data;
+                    $jsonArray['error']   = $error;
+                }
+            }
+            
             header('Content-Type: application/json');
             echo json_encode($jsonArray);
-        } /**
+        }
+        
+        /**
          *  Front End Routes
-         * */
+         *
+         */
         else {
             if ($path[0] == '') {
                 require 'front/index.html';
+                
             } else if ($path[0] == 'article') {
-
                 require 'front/article.html';
+                
             }
         }
-//
-
-
+        
+        //
+        
     }
-
-
 }
+?> Back
